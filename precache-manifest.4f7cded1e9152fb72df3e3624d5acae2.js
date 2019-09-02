99a8517208f5cfb99d79647b2659aa4b32631053
Kannan Balasubramanian <rbkannan1@gmail.com>
2019-09-02 19:22:15 +0530

    Updates

---
+++ precache-manifest.4f7cded1e9152fb72df3e3624d5acae2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b0e237f56c4939056039ee13916ed68",
+    "url": "/todoApp-react/index.html"
+  },
+  {
+    "revision": "c67da38455151b07e30b",
+    "url": "/todoApp-react/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "757cfa967e2b0fbf5d2e",
+    "url": "/todoApp-react/static/css/main.b3a57e2e.chunk.css"
+  },
+  {
+    "revision": "c67da38455151b07e30b",
+    "url": "/todoApp-react/static/js/2.005c2524.chunk.js"
+  },
+  {
+    "revision": "757cfa967e2b0fbf5d2e",
+    "url": "/todoApp-react/static/js/main.8f7f6b2b.chunk.js"
+  },
+  {
+    "revision": "dbbbe2a6f9243ed029fc",
+    "url": "/todoApp-react/static/js/runtime~main.ad548b91.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todoApp-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
